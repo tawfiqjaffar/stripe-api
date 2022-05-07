59a4b6ba516abc59e59b06f68f899360f3d0c0d2
Tawfiq JAFFAR <tawfiq.jaffar@joza-it.fr>
2022-05-07 22:43:25 +0200

    added project

---
+++ src/main/java/com/tawfiqjaffar/domain/CreatePayment.java
@@ -0,0 +1,9 @@
+package com.tawfiqjaffar.domain;
+
+public class CreatePayment<T> {
+    private T content;
+
+    public T getContent() {
+        return content;
+    }
+}
---
+++ src/main/java/com/tawfiqjaffar/service/PaymentService.java
@@ -0,0 +1,68 @@
+package com.tawfiqjaffar.service;
+
+import com.google.gson.JsonSyntaxException;
+import com.stripe.Stripe;
+import com.stripe.exception.SignatureVerificationException;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Event;
+import com.stripe.model.EventDataObjectDeserializer;
+import com.stripe.model.PaymentIntent;
+import com.stripe.model.StripeObject;
+import com.stripe.net.Webhook;
+import com.stripe.param.PaymentIntentCreateParams;
+import com.tawfiqjaffar.domain.CreatePayment;
+import com.tawfiqjaffar.listener.StripeEventListener;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+public class PaymentService<CreatePaymentResponseType, PaymentHistoryContentType> {
+
+    private final String webhookKey;
+
+    private final StripeEventListener<PaymentHistoryContentType> stripeEventListener;
+
+    public PaymentService(final String apiKey, final String webhookKey, final StripeEventListener<PaymentHistoryContentType> stripeEventListener) {
+        this.webhookKey = webhookKey;
+        this.stripeEventListener = stripeEventListener;
+        Stripe.apiKey = apiKey;
+
+    }
+
+    public void createPaymentIntent(final CreatePayment<PaymentHistoryContentType> createPayment, final Long amount, final String currency) {
+        PaymentIntentCreateParams createParams = new PaymentIntentCreateParams.Builder().setCurrency(currency).setAmount(amount).build();
+        PaymentIntent intent;
+        try {
+            intent = PaymentIntent.create(createParams);
+            this.stripeEventListener.onStripeIntentCreated(intent.getId(), intent.getStatus(), createPayment.getContent());
+        } catch (StripeException e) {
+            this.stripeEventListener.onStripeError(e);
+        }
+    }
+
+    public void webhook(final String payload, final String sigHeader) {
+        Event event;
+
+        try {
+            event = Webhook.constructEvent(payload, sigHeader, this.webhookKey);
+        } catch (JsonSyntaxException | SignatureVerificationException e) {
+            this.stripeEventListener.onStripeError(e);
+            return;
+        }
+        PaymentIntent intent = (PaymentIntent) event.getDataObjectDeserializer().getObject().get();
+        EventDataObjectDeserializer dataObjectDeserializer = event.getDataObjectDeserializer();
+        StripeObject stripeObject;
+        if (dataObjectDeserializer.getObject().isPresent()) {
+            stripeObject = dataObjectDeserializer.getObject().get();
+            this.stripeEventListener.onStripeLog(stripeObject.toString());
+        } else {
+            this.stripeEventListener.onStripeError(new Exception("Stripe event deserialization failed"));
+            return;
+        }
+        this.stripeEventListener.onStripePaymentStatusUpdated(intent.getId(), event.getType(), intent.getReceiptEmail());
+
+        if (event.getType().equals("payment_intent.succeeded")) {
+            this.stripeEventListener.onStripePaymentSucceeded();
+        }
+    }
+}
