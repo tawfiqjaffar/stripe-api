7ad8b59f4bcfc3f9b14c22705117040e51fa10b9
Tawfiq JAFFAR <tawfiq.jaffar@joza-it.fr>
2022-05-07 23:41:59 +0200

    added stripe webhook listener

--- src/main/java/com/tawfiqjaffar/service/PaymentService.java
+++ src/main/java/com/tawfiqjaffar/service/PaymentService.java
@@ -11,18 +11,15 @@ import com.stripe.model.StripeObject;
 import com.stripe.net.Webhook;
 import com.stripe.param.PaymentIntentCreateParams;
 import com.tawfiqjaffar.domain.CreatePayment;
-import com.tawfiqjaffar.listener.StripeEventListener;
 import com.tawfiqjaffar.listener.StripeIntentListener;
+import com.tawfiqjaffar.listener.StripeWebhookListener;
 
 public class PaymentService<CreatePaymentResponseType, PaymentHistoryContentType> {
 
     private final String webhookKey;
 
-    private final StripeEventListener<PaymentHistoryContentType> stripeEventListener;
-
-    public PaymentService(final String apiKey, final String webhookKey, final StripeEventListener<PaymentHistoryContentType> stripeEventListener) {
+    public PaymentService(final String apiKey, final String webhookKey) {
         this.webhookKey = webhookKey;
-        this.stripeEventListener = stripeEventListener;
         Stripe.apiKey = apiKey;
 
     }
@@ -38,13 +35,13 @@ public class PaymentService<CreatePaymentResponseType, PaymentHistoryContentType
         }
     }
 
-    public void webhook(final String payload, final String sigHeader) {
+    public void webhook(final String payload, final String sigHeader, StripeWebhookListener stripeWebhookListener) {
         Event event;
 
         try {
             event = Webhook.constructEvent(payload, sigHeader, this.webhookKey);
         } catch (JsonSyntaxException | SignatureVerificationException e) {
-            this.stripeEventListener.onStripeError(e);
+            stripeWebhookListener.onStripeError(e);
             return;
         }
         PaymentIntent intent = (PaymentIntent) event.getDataObjectDeserializer().getObject().get();
@@ -52,15 +49,15 @@ public class PaymentService<CreatePaymentResponseType, PaymentHistoryContentType
         StripeObject stripeObject;
         if (dataObjectDeserializer.getObject().isPresent()) {
             stripeObject = dataObjectDeserializer.getObject().get();
-            this.stripeEventListener.onStripeLog(stripeObject.toString());
+            stripeWebhookListener.onStripeLog(stripeObject.toString());
         } else {
-            this.stripeEventListener.onStripeError(new Exception("Stripe event deserialization failed"));
+            stripeWebhookListener.onStripeError(new Exception("Stripe event deserialization failed"));
             return;
         }
-        this.stripeEventListener.onStripePaymentStatusUpdated(intent.getId(), event.getType(), intent.getReceiptEmail());
+        stripeWebhookListener.onStripePaymentStatusUpdated(intent.getId(), event.getType(), intent.getReceiptEmail());
 
         if (event.getType().equals("payment_intent.succeeded")) {
-            this.stripeEventListener.onStripePaymentSucceeded();
+            stripeWebhookListener.onStripePaymentSucceeded();
         }
     }
 }
